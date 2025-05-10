c6f593943b26293ba70b5548065a081747dd1c2c
Churchill <120373321+lifeofchurchill@users.noreply.github.com>
2025-05-10 15:41:57 +0000

    Human choice function for the rock paper scissors game

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-console.log('hello world')
-function getComputerChoice () {
-    const randomNumber = Math.random()
 
-    if (randomNumber < 0.37) {
+function getComputerChoice () {
+    const randomNumber = Math.floor(Math.random() * 50)
+
+    if (randomNumber < 20) {
         return "rock";
-    } else if (randomNumber < 0.68) {
+    } else if (randomNumber < 35) {
     return "paper";
     } else {
     return "scissors";
     }
 }
+    console.log(getComputerChoice())
+
+function getHumanChoice () {
+    let input = prompt("Enter a number between 1 and 50 compete with computer")
+    input = Number(input)
+
+    if (input <= 20) {
+        return "rock";
+    } else if (input <= 35) {
+        return "paper";
+    }
+    else if (input <= 50) {
+        return "Scissors"
+    }
+    else {
+        return "Invalid input. please enter a number between 1 and 50"
+    }
+}
+   console.log(getHumanChoice())
